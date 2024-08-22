4394cc94f10c206fd3619e3a46ac85c2c034ee2c
VictorMarimon <3reehuy@gmail.com>
2024-08-22 17:02:20 -0500

    feat: :sparkles: se extrae la información de la API de los personajes

--- js/personajes.js
+++ js/personajes.js
@@ -15,36 +15,32 @@ fetch("https://swapi.py4e.com/api/people")
     console.error("Hubo un error en el JSON " + res);
   });
 
+
+//la siguiente función nos permite desestructurar la url de cada personaje para posteriormente obtener su imágen
+
+function idCharacter(url){
+  const pieces = url.split('/');
+  return pieces[pieces.length - 2];
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   var bandera = 0;
   setTimeout(() => {
     const content = document.querySelector('.cards-grid')
-    for (let i = 0; i < 10; i++) {
-      for (const dato of Array(PERSONAJES)) {
-        const card = document.createElement('div');
-        const imgCharacter = document.createElement('img');
-        const contentCard = document.createElement('div');
-        const tittleCard = document.createElement('h3');
-        const heighCard = document.createElement('p');
-        const birthYearCard = document.createElement('p');
-        const genderCard = document.createElement('p');
+    for (const dato of PERSONAJES.results) {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+                    <img src="https://starwars-visualguide.com/assets/img/characters/${idCharacter(dato.url)}.jpg" alt="${(dato.name)}">
+                    <div class="card-content">
+                        <h3>${dato.name}</h3>
+                        <p>${dato.height + " cm"}</p>
+                        <p>${dato.birth_year}</p>
+                        <p>${dato.gender}</p>
+                    </div>
+      `;
 
-        card.classList.add('card');
-
-        contentCard.classList.add('card-content');
-
-        card.appendChild(imgCharacter);
-        card.appendChild(contentCard);
-        contentCard.appendChild(tittleCard);
-        contentCard.appendChild(heighCard);
-        contentCard.appendChild(birthYearCard);
-        contentCard.appendChild(genderCard);
-
-        content.append(card);
-
-        console.log(dato.results[bandera][bandera]);
-        
-      }
+      content.append(card);
       bandera++;
     }
   }, 200);
